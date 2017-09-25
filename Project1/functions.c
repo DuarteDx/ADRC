fa78d05b86d84f8e23191a80ec192641cf8c6e69
Duarte Dias <DuartexDias@gmail.com>
2017-09-25 17:44:16 +0100

    Initial file structure for project1

---
+++ Project1/functions.c
@@ -0,0 +1,30 @@
+#include "header.h"
+
+//Basic functions
+void PrefixTree(){
+
+}
+
+void PrintTable(){
+
+}
+
+int LookUp(){
+
+  return 0;
+}
+
+int DeletePrefix(){
+
+  return 0;
+}
+
+//Extra functions
+int BinaryToTwoBit(){
+
+  return 0;
+}
+
+void PrintTableEven(){
+
+}
---
+++ Project1/header.h
@@ -0,0 +1,11 @@
+
+//Basic functions
+void PrefixTree();
+void PrintTable();
+int LookUp();
+int InsertPrefix();
+int DeletePrefix();
+
+//Extra functions for extra point
+int BinaryToTwoBit();
+void PrintTableEven();
