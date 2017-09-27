1296cc3b1f946b2129f6fb4744981646063b7d9c
FranciscoPombal <pombal.francisco@gmail.com>
2017-09-27 15:30:12 +0100

    Start writing necessary tree functions

--- Project1/include/tree.h
+++ Project1/include/tree.h
@@ -1,10 +1,11 @@
 #include "defs.h"
+#include "../include/table.h"
 
-typedef struct treenode TreeNode;
-typedef struct treenode_2 TreeNode_2;
+typedef struct treeNode * TreeNode;
+typedef struct treeNode_2 * TreeNode_2;
 
 //Basic functions
-void PrefixTree();
+TreeNode PrefixTree(TableEntry table_head);
 void PrintTable();
 int LookUp();
 int InsertPrefix();
--- Project1/src/tree.c
+++ Project1/src/tree.c
@@ -1,24 +1,50 @@
 #include "../include/tree.h"
 
-struct treenode
+struct treeNode
 {
-    Item this;
+    int nextHop;
     struct treeNode * zero;
     struct treeNode * one;
 };
 
-struct treenode_2
+struct treeNode_2
 {
-    Item this;
+    int nextHop;
     struct treenode_2 * zero;
     struct treenode_2 * one;
     struct treenode_2 * two;
     struct treenode_2 * three;
 };
 
-//Basic functions
-void PrefixTree(){
 
+int TreeNode_getNextHop(TreeNode treeNode)
+{
+    return treeNode->nextHop;
+}
+
+TreeNode TreeNode_getZero(TreeNode treeNode)
+{
+    return treeNode->zero;
+}
+
+TreeNode TreeNode_getOne(TreeNode treeNode)
+{
+    return treeNode->one;
+}
+
+//Basic functions
+TreeNode PrefixTree(TableEntry table_head){
+
+    TreeNode tree_root = NULL;
+    TableEntry aux = NULL;
+    int i = 0;
+
+        for(aux = table_head; aux != NULL; aux = TableEntry_getNextTableEntry(aux)) {
+            if(strncmp(TableEntry_getPrefix(aux), "e", PREFIX_SIZE) == 0){
+            }
+        }
+
+    return tree_root;
 }
 
 void PrintTable(){
