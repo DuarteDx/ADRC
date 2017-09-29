1ed4b3852cb2c2ecde56fc6af7d60ba56e9ab243
FranciscoPombal <pombal.francisco@gmail.com>
2017-09-29 01:53:47 +0100

    Start adding calls to main

--- Project1/src/main.c
+++ Project1/src/main.c
@@ -7,7 +7,10 @@ int main(int argc, char const *argv[]) {
     int choice = 0;
     FILE* fp = NULL;
     TableEntry* table_head = NULL;
+    TreeNode* tree_root = NULL;
     bool keepRunning = true;
+    char address[PREFIX_SIZE];
+    int i = 0;
 
         // Check for correct number of arguments
         if(argc != 2){
@@ -22,18 +25,22 @@ int main(int argc, char const *argv[]) {
             exit(EXIT_FAILURE);
         }
 
-        table_head = readTable(fp);
-        printTable(table_head);
-
         while(true == keepRunning){
             choice = showMenu();
             switch(choice){
                 case 1:
                 {
+                    table_head = readTable(fp);
+                    printTable(table_head);
+                    tree_root = PrefixTree(table_head);
                     break;
                 }
                 case 2:
                 {
+                    if(NULL == tree_root){
+                        fprintf(stdout, "No tree to print!\n");
+                    }
+                    PrintTable(tree_root, address);
                     break;
                 }
                 case 3:
