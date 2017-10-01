352c6ed55213ac78f162480fe8b163993a5d6e0b
FranciscoPombal <pombal.francisco@gmail.com>
2017-10-01 20:44:09 +0100

    need to add a function to free the tree

--- Project1/include/tree.h
+++ Project1/include/tree.h
@@ -5,16 +5,18 @@ typedef struct treeNode_ TreeNode;
 typedef struct treeNode_2_ TreeNode_2;
 
 //Basic functions
-TreeNode* PrefixTree(TableEntry *);
+TreeNode * PrefixTree(TableEntry *);
 void PrintTable(TreeNode *, char address[PREFIX_SIZE]);
 int LookUp(TreeNode *, char *);
-TreeNode* InsertPrefix(TreeNode *, char *, int);
-TreeNode* DeletePrefix(TreeNode *, char *);
+TreeNode * InsertPrefix(TreeNode *, char *, int);
+TreeNode * DeletePrefix(TreeNode *, char *);
 void TreeNode_setZero(TreeNode *treeNode, TreeNode *nextZero);
 void TreeNode_setOne(TreeNode *treeNode, TreeNode *nextOne);
 void TreeNode_setNextHop(TreeNode *treeNode, int nextHop);
-TreeNode* newTreeNode(void);
+TreeNode * newTreeNode(void);
+void freeTree(TreeNode * tree_root);
 
 //Extra functions for extra point
-int BinaryToTwoBit();
+
+void BinaryToTwoBit();
 void PrintTableEven();
