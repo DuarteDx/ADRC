16874e5070e17b254b3e761b04e4741052ba362f
FranciscoPombal <pombal.francisco@gmail.com>
2017-10-21 19:07:05 +0100

    Create Initial files for project 2

---
+++ Project2/include/defs.h
@@ -0,0 +1,23 @@
+#ifndef _DEFS_H_
+#define _DEFS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+/* CHAR_BUFFER_SIZE
+ * Description: generic text input buffer size limit
+*/
+#ifndef CHAR_BUFFER_SIZE
+#define  CHAR_BUFFER_SIZE 512
+#endif
+
+/* PREFIX_SIZE
+ * Description: IPv6 addresses are 128 bits in length, so prefixes will never exceed 128 bits
+ */
+#ifndef PREFIX_SIZE
+#define PREFIX_SIZE 128
+#endif
+
+#endif
---
+++ Project2/src/main.c
@@ -0,0 +1,6 @@
+#include "../include/defs.h"
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
